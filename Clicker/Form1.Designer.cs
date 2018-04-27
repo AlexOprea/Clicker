5943e14b850ae50a3496049570ad9f10b5ddac0c
aoprea <StefanAlexandru.Oprea@stefanini.com>
2018-04-27 09:33:16 +0300

    add project files

---
+++ Clicker/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Clicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxLC = new System.Windows.Forms.CheckBox();
+            this.checkBoxRC = new System.Windows.Forms.CheckBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.freqValue = new System.Windows.Forms.NumericUpDown();
+            this.debugLog = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.freqValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxLC
+            // 
+            this.checkBoxLC.AutoSize = true;
+            this.checkBoxLC.Location = new System.Drawing.Point(24, 49);
+            this.checkBoxLC.Name = "checkBoxLC";
+            this.checkBoxLC.Size = new System.Drawing.Size(69, 17);
+            this.checkBoxLC.TabIndex = 0;
+            this.checkBoxLC.Text = "Left click";
+            this.checkBoxLC.UseVisualStyleBackColor = true;
+            this.checkBoxLC.CheckedChanged += new System.EventHandler(this.checkBoxLC_CheckedChanged);
+            // 
+            // checkBoxRC
+            // 
+            this.checkBoxRC.AutoSize = true;
+            this.checkBoxRC.Location = new System.Drawing.Point(24, 81);
+            this.checkBoxRC.Name = "checkBoxRC";
+            this.checkBoxRC.Size = new System.Drawing.Size(76, 17);
+            this.checkBoxRC.TabIndex = 1;
+            this.checkBoxRC.Text = "Right click";
+            this.checkBoxRC.UseVisualStyleBackColor = true;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(24, 126);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 2;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(130, 126);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(75, 23);
+            this.stopBtn.TabIndex = 3;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Frequency(seconds)";
+            // 
+            // freqValue
+            // 
+            this.freqValue.DecimalPlaces = 1;
+            this.freqValue.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.freqValue.Location = new System.Drawing.Point(130, 105);
+            this.freqValue.Name = "freqValue";
+            this.freqValue.Size = new System.Drawing.Size(120, 20);
+            this.freqValue.TabIndex = 5;
+            this.freqValue.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            // 
+            // debugLog
+            // 
+            this.debugLog.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.debugLog.Location = new System.Drawing.Point(0, 155);
+            this.debugLog.Multiline = true;
+            this.debugLog.Name = "debugLog";
+            this.debugLog.ReadOnly = true;
+            this.debugLog.Size = new System.Drawing.Size(426, 73);
+            this.debugLog.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(278, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Developed by Anworth";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 228);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.debugLog);
+            this.Controls.Add(this.freqValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.checkBoxRC);
+            this.Controls.Add(this.checkBoxLC);
+            this.Name = "Form1";
+            this.Text = "Clicker v1";
+            ((System.ComponentModel.ISupportInitialize)(this.freqValue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxLC;
+        private System.Windows.Forms.CheckBox checkBoxRC;
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown freqValue;
+        private System.Windows.Forms.TextBox debugLog;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Clicker/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsInput;
+
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool execute = false;
+
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const int MOUSEEVENTF_LEFTUP = 0x0004;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        Timer timer = new Timer();
+        private InputSimulator inputSimulator = new InputSimulator();
+        private Process[] processes = null;
+        private IntPtr process;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool PostMessage(int hWnd, uint Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        private static extern int SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        // Get a handle to an application window.
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+
+        private void checkBoxLC_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            if (processes == null)
+            {
+                processes = Process.GetProcessesByName("Conquer");
+            }
+            if (processes != null && processes.Length > 0)
+            {
+                process = processes[0].MainWindowHandle;
+                SetForegroundWindow(process);
+                debugLog.AppendText("Found \n");
+            }
+            float freValue = (float)freqValue.Value;
+            int valueToSleep = (int)(1000 * freValue);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = valueToSleep;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            if (checkBoxLC.Checked )
+            {
+                
+                // inputSimulator.Mouse.LeftButtonDown();
+                // inputSimulator.Mouse.LeftButtonUp();
+                //PostMessage(process, MOUSEEVENTF_LEFTDOWN| MOUSEEVENTF_LEFTUP, 0, 1);
+                SendMessage(process, MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, (IntPtr)0, (IntPtr)0);
+                // PostMessage(process, MOUSEEVENTF_LEFTUP| MOUSEEVENTF_ABSOLUTE, 0, 1);
+
+            }
+            if (checkBoxRC.Checked)
+            {
+                //inputSimulator.Mouse.RightButtonDown();
+                //inputSimulator.Mouse.RightButtonUp();
+                //PostMessage(process, MOUSEEVENTF_RIGHTDOWN| MOUSEEVENTF_RIGHTUP, 0, 1);
+                // PostMessage(process, MOUSEEVENTF_RIGHTUP| MOUSEEVENTF_ABSOLUTE, 0, 1);
+                SendMessage(process, MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP, (IntPtr)0, (IntPtr)0);
+            }
+        }
+
+    }
+}
